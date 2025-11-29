4d49f526250f84e99d35e0589180205f67e9276c
Kinderek <sejudek12@gmail.com>
2025-11-29 17:03:50 +0100

    Dodano testy jednostkowe dla klasy MergeSort

--- Testy2/tests.cpp
+++ Testy2/tests.cpp
@@ -52,3 +52,86 @@ TEST(MergeSortTests, PositiveAndNegativeNumbers) {
 
     EXPECT_EQ(arr, expected);
 }
+
+TEST(MergeSortTests, EmptyArray) {
+    std::vector<int> arr = {};
+    std::vector<int> expected = {};
+
+    MergeSort<int>::sort(arr);
+
+    EXPECT_EQ(arr, expected);
+}
+
+TEST(MergeSortTests, SingleElementArray) {
+    std::vector<int> arr = { 42 };
+    std::vector<int> expected = { 42 };
+
+    MergeSort<int>::sort(arr);
+
+    EXPECT_EQ(arr, expected);
+}
+
+TEST(MergeSortTests, TwoElementsArray) {
+    std::vector<int> arr = { 20, 10 };
+    std::vector<int> expected = { 10, 20 };
+
+    MergeSort<int>::sort(arr);
+
+    EXPECT_EQ(arr, expected);
+}
+
+TEST(MergeSortTests, ArrayWithDuplicates) {
+    std::vector<int> arr = { 5, 2, 8, 2, 5, 1 };
+    std::vector<int> expected = { 1, 2, 2, 5, 5, 8 };
+    MergeSort<int>::sort(arr);
+
+    EXPECT_EQ(arr, expected);
+}
+
+TEST(MergeSortTests, NegativeArrayWithDuplicates) {
+    std::vector<int> arr = { -5, -2, -8, -2, -5, -1 };
+    std::vector<int> expected = { -8, -5, -5, -2, -2, -1 };
+
+    MergeSort<int>::sort(arr);
+
+    EXPECT_EQ(arr, expected);
+}
+
+TEST(MergeSortTests, MixedArrayWithDuplicates) {
+    std::vector<int> arr = { 0, -10, 5, 0, 10, -5, 5, 10 };
+    std::vector<int> expected = arr;
+    std::sort(expected.begin(), expected.end());
+    MergeSort<int>::sort(arr);
+
+    EXPECT_EQ(arr, expected);
+}
+
+TEST(MergeSortTests, LargeArray) {
+    std::vector<int> arr;
+    for (int i = 200; i >= 0; --i) {
+        arr.push_back(i);
+    }
+
+    std::vector<int> expected = arr;
+    std::sort(expected.begin(), expected.end());
+
+    MergeSort<int>::sort(arr);
+
+    EXPECT_EQ(arr, expected);
+}
+    
+
+TEST(MergeSortTests, LargeMixedArrayWithDuplicates) {
+    std::vector<int> arr;
+    for (int i = -150; i <= 150; ++i) {
+        arr.push_back(i);
+        if (i % 10 == 0) arr.push_back(i); 
+    }
+
+    std::vector<int> expected = arr;
+    std::sort(expected.begin(), expected.end());
+
+    MergeSort<int>::sort(arr);
+
+    EXPECT_EQ(arr, expected);
+}
